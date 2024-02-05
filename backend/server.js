2e512ae6e04df4ea7de854be001f9225ef1c9d0d
en-arnob <en.arnob@gmail.com>
2024-02-05 21:13:23 +0600

    Project Struct

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+// connect db and start server
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Database connected and sever running at ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
