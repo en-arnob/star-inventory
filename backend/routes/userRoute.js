3fab187cfd5ab41246d68f8fb8a6726b8c047e26
en-arnob <en.arnob@gmail.com>
2024-02-10 04:27:53 +0600

    Auth Middleware

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,10 +1,12 @@
 module.exports = (app) => {
   const userRouter = require("express").Router();
   const userController = require("../controllers/userController");
+  const { protected } = require("../middlewares/authMiddleware");
 
   userRouter.post("/register", userController.register);
   userRouter.post("/login", userController.login);
   userRouter.get("/logout", userController.logout);
+  userRouter.get("/get-user-data", protected, userController.getUserData);
 
   app.use("/api/user", userRouter);
 };
